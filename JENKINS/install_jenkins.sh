e64e56b08f96d03ed56eb964f1dd0bd60da0299d
adulojutope <Adulojutope3016@gmail.com>
2025-04-04 19:43:43 -0500

    Update install_jenkins.sh

--- JENKINS/install_jenkins.sh
+++ JENKINS/install_jenkins.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 sudo apt update
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 63667EE74BBA1F0A08A698725BA31D57EF5975CA
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
@@ -26,4 +26,6 @@ echo 'clearing screen...' && sleep 5
 clear
 echo 'jenkins is installed'
 echo 'this is the default password :' $(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
-sudo reboot
+# start Jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
